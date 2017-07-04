e33089cafcc9fd2ba1011b0450b4d3a3dd34243e
Alexandre BROCARD <ab.nightowl33@gmail.com>
2017-07-04 11:38:57 +0200

    git init + création programme Bonjour.java et fichier jar + javadoc

---
+++ org/dta/Bonjour.java
@@ -0,0 +1,22 @@
+package org.dta;
+
+// Methode Bonjour 
+/* 
+pour afficher un message de bienvenue
+*/
+
+/** 
+Bonjour <br> 
+Prend des strings en arguments (ex: un prenom) 
+et renvoie une phrase commencant par "Bonjour" + le 1er element string 
+*/
+
+public class Bonjour {
+    public static void main(String[] args) {
+
+        System.out.println("Bonjour " + args[0]);
+
+    }
+
+}
+
